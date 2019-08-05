e9d6fe1ec3bec711ff9d56be7659d66657ab1481
leejuneui <wnsspdl@naver.com>
2019-08-05 15:03:58 +0900

    스택, 큐, 덱 완료

---
+++ 알고리즘연습/BarkingDog/3강/스택수열.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <stack>
+#include <vector>
+using namespace std;
+
+class Sequence
+{
+	vector<char> s;
+public:
+	bool is_possible(vector<int>&);
+	void print();
+};
+
+bool Sequence::is_possible(vector<int>& a) {
+	stack<int> st;
+	int num = 1;
+	for (int i = 0; i < a.size(); i++) {
+		while (true) {
+			if (num <= a[i]) {
+				st.push(num++);
+				s.push_back('+');
+			}
+			else {
+				if (a[i] == st.top()) {
+					st.pop();
+					s.push_back('-');
+					break;
+				}
+				else return false;
+			}
+		}
+	}
+	return true;
+}
+
+void Sequence::print() {
+	for (int i = 0; i < s.size(); i++) {
+		printf("%c\n", s[i]);
+	}
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	vector<int> data;
+	for (int i = 0; i < n; i++) {
+		int t;
+		scanf("%d", &t);
+		data.push_back(t);
+	}
+	Sequence seq;
+	if (seq.is_possible(data)) {
+		seq.print();
+	}
+	else {
+		printf("NO\n");
+	}
+}
