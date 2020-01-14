770e448f3c32a74b6d6924c9d3e69f715283cf1f
coconut-desktop <wnsspdl@naver.com>
2020-01-14 12:29:16 +0900

    수정 : 소수찾기 코딩 스타일 수정

--- 알고리즘연습/프로그래머스/완전탐색/소수찾기.cpp
+++ 알고리즘연습/프로그래머스/완전탐색/소수찾기.cpp
@@ -11,18 +11,24 @@ int prime_array[10000000];
 int primes[10000000];
 int answer;
 
-void Getche (int num) {
+void Getche (int num) 
+{
     int i;
-    for (i = 2; i <= num; ++i) {
+    for (i = 2; i <= num; ++i) 
+    {
         primes[i] = i;
     }
 
-    for (i = 2; i <= num; ++i) {
-        if (primes[i] == 0) {
+    for (i = 2; i <= num; ++i) 
+    {
+        if (primes[i] == 0) 
+        {
             continue;
         }
-        else {
-            for (int j = i + i; j <= num; j += i) {
+        else 
+        {
+            for (int j = i + i; j <= num; j += i) 
+            {
                 primes[j] = 0;
             }
         }
@@ -30,34 +36,42 @@ void Getche (int num) {
     }
 }
 
-void Bruteforce (int m, string prime, int index) {
-    if (m == prime.length ()) {
-        if (prime_array[stoi (prime)]) {
+void Bruteforce (int m, string prime, int index) 
+{
+    if (m == prime.length()) 
+    {
+        if (prime_array[stoi(prime)]) 
+        {
             return;
         }
-        else {
-            prime_array[stoi (prime)] = 1;
-            if (primes[stoi (prime)] != 0) answer++;
+        else 
+        {
+            prime_array[stoi(prime)] = 1;
+            if (primes[stoi(prime)] != 0) answer++;
         }
         return;
     }
-    else {
-        for (int i = 0; i < global_numbers.length (); ++i) {
+    else 
+    {
+        for (int i = 0; i < global_numbers.length(); ++i) 
+        {
             if (check[i]) continue;
             check[i] = 1;
-            Bruteforce (m, prime + global_numbers[i], i);
+            Bruteforce(m, prime + global_numbers[i], i);
             check[i] = 0;
         }
     }
 }
 
-int solution (string numbers) {
-    memset (check, 0, sizeof (check));
+int solution (string numbers) 
+{
+    memset(check, 0, sizeof (check));
     global_numbers = numbers;
     answer = 0;
-    Getche (pow (10, numbers.length ()));
-    for (int i = 1; i <= numbers.length (); ++i) {
-        Bruteforce (i, "", 0);
+    Getche(pow(10, numbers.length()));
+    for (int i = 1; i <= numbers.length(); ++i) 
+    {
+        Bruteforce(i, "", 0);
     }
     return answer;
 }
